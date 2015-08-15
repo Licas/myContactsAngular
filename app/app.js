cddc2c040dea32ace8ad412a695bf16151efc52f
Manuel Morini <morinimanuel@gmail.com>
2015-08-15 20:39:19 +0200

    basic add contact form and functions

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myContacts', [
-  'ngRoute'
+    'ngRoute',
+    'myContacts.contacts',
+    'firebase'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/contacts'});
--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -1,14 +1,28 @@
 'use strict';
 
-angular.module('myContacts.view1', ['ngRoute'])
+angular.module('myContacts.contacts', ['ngRoute','firebase'])
+
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'ContactsCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function(scope, fbArr) {
+    var ref = new Firebase('https://mycontactsangularjs.firebaseio.com/contacts');
+
+    scope.contacts = fbArr(ref);
+
+    scope.showAddContactForm = function(){
+        scope.addFormShow = true;
+    }
+
+    scope.exitAddContactForm = function(){
+        scope.addFormShow = false;
+    }
+
 
 }]);
